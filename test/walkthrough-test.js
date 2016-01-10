b0811949fe71c8a7218104032721cc3d01bb3198
Gregor Martynus <gregor@martynus.net>
2016-01-10 13:09:06 +0100

    style: standard

--- test/walkthrough-test.js
+++ test/walkthrough-test.js
@@ -1,7 +1,7 @@
-/* global describe, it */
+/* global describe, it, hoodie */
 require('@gr2m/frontend-test-setup')
 
-function toValue(result) {
+function toValue (result) {
   if (isError(result.value)) {
     var error = new Error(result.value.message)
     Object.keys(result.value).forEach(function (key) {
@@ -33,11 +33,11 @@ describe('my demo page', function () {
   })
 
   it('walkthrough', function () {
-    username = 'user' + Math.random().toString(16).substr(2)
+    var username = 'user' + Math.random().toString(16).substr(2)
 
     return this.client.url('/')
 
-      .executeAsync(function storeAdd(username, done) {
+      .executeAsync(function storeAdd (username, done) {
         return hoodie.store.add({
           foo: 'bar'
         })
@@ -46,7 +46,7 @@ describe('my demo page', function () {
       }, username).then(toValue)
       .should.eventually.have.property('foo', 'bar')
 
-      .executeAsync(function signUp(username, done) {
+      .executeAsync(function signUp (username, done) {
         return hoodie.account.signUp({
           username: username,
           password: 'secret'
@@ -56,7 +56,7 @@ describe('my demo page', function () {
       }, username).then(toValue)
       .should.eventually.have.property('username', username)
 
-      .executeAsync(function signOut(username, done) {
+      .executeAsync(function signOut (username, done) {
         return hoodie.account.signIn({
           username: username,
           password: 'secret'
@@ -67,12 +67,12 @@ describe('my demo page', function () {
       .should.eventually.have.property('username', username)
 
       .waitUntil(function () {
-        return this.execute(function storeHasNoLocalChanges() {
+        return this.execute(function storeHasNoLocalChanges () {
           return hoodie.store.hasLocalChanges() === false
         })
       }, 10000)
 
-      .executeAsync(function signOut(done) {
+      .executeAsync(function signOut (done) {
         return hoodie.account.signOut()
 
         .then(done, done)
@@ -85,7 +85,6 @@ describe('my demo page', function () {
       //   .then(done, done)
       // }).then(toValue)
       // .should.eventually.be.empty
-
 
       .catch(logError)
   })
