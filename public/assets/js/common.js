0dd33c71827ccf563b38a487304f7e3d11ba294c
Gregor <gregor@martynus.net>
2016-12-22 19:15:51 -0800

    fix: adapt for async hoodie.ready usage
    see hoodiehq/camp#101

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,20 +1,22 @@
 /* global hoodie */
-function showSignedIn (username) {
-  document.querySelector('[data-value=username]').textContent = username
-  document.body.setAttribute('data-account-state', 'signed-in')
-}
+hoodie.ready.then(function () {
+  function showSignedIn (username) {
+    document.querySelector('[data-value=username]').textContent = username
+    document.body.setAttribute('data-account-state', 'signed-in')
+  }
 
-function hideSignedIn () {
-  document.body.setAttribute('data-account-state', 'signed-out')
-}
+  function hideSignedIn () {
+    document.body.setAttribute('data-account-state', 'signed-out')
+  }
 
-hoodie.account.on('signin', function (account) {
-  showSignedIn(account.username)
+  hoodie.account.on('signin', function (account) {
+    showSignedIn(account.username)
+  })
+
+  hoodie.account.on('signout', hideSignedIn)
+  if (hoodie.account.isSignedIn()) {
+    showSignedIn(hoodie.account.username)
+  } else {
+    hideSignedIn()
+  }
 })
-
-hoodie.account.on('signout', hideSignedIn)
-if (hoodie.account.isSignedIn()) {
-  showSignedIn(hoodie.account.username)
-} else {
-  hideSignedIn()
-}
