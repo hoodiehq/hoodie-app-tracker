2757cbd742016ddcb483ae9ef1d7cf16c302f794
Gregor <gregor@martynus.net>
2016-12-22 19:15:51 -0800

    fix: adapt for async hoodie.ready usage
    see hoodiehq/camp#101

--- public/assets/js/account.js
+++ public/assets/js/account.js
@@ -1,206 +1,207 @@
-var originalDocumentTitle = document.title
-var locationHash = window.location.hash.replace('#', '')
+hoodie.ready.then(function () {
+  var originalDocumentTitle = document.title
+  var locationHash = window.location.hash.replace('#', '')
 
-/**
- * Find all elements with a data-show attribute and apply a click handler
- * in which we check if an element with a data-action attribute was clicked.
- * If yes, then set the data-show attribute of the container element to what
- * is in the data-action of the clicked attribute (after the show- prefix)
- */
-;[].forEach.call(document.querySelectorAll('[data-show]'), function (el) {
-  el.addEventListener('click', function (event) {
-    var action = event.target.dataset.action
-    var $formsContainer = event.currentTarget
+  /**
+   * Find all elements with a data-show attribute and apply a click handler
+   * in which we check if an element with a data-action attribute was clicked.
+   * If yes, then set the data-show attribute of the container element to what
+   * is in the data-action of the clicked attribute (after the show- prefix)
+   */
+  ;[].forEach.call(document.querySelectorAll('[data-show]'), function (el) {
+    el.addEventListener('click', function (event) {
+      var action = event.target.dataset.action
+      var $formsContainer = event.currentTarget
 
-    if (!action) {
-      return
+      if (!action) {
+        return
+      }
+
+      event.preventDefault()
+      var showTarget = action.substr('show-'.length)
+      $formsContainer.setAttribute('data-show', showTarget)
+      setHashState(showTarget)
+    })
+    if (locationHash) {
+      el.setAttribute('data-show', locationHash)
+      setHashState(locationHash)
     }
+  })
 
+  /**
+   * Handle signup form submit
+   */
+  document.querySelector('form.signup').addEventListener('submit', function (event) {
     event.preventDefault()
-    var showTarget = action.substr('show-'.length)
-    $formsContainer.setAttribute('data-show', showTarget)
-    setHashState(showTarget)
-  })
-  if (locationHash) {
-    el.setAttribute('data-show', locationHash)
-    setHashState(locationHash)
-  }
-})
 
-/**
- * Handle signup form submit
- */
-document.querySelector('form.signup').addEventListener('submit', function (event) {
-  event.preventDefault()
+    var email = this.querySelector('[name=email]').value
+    var password = this.querySelector('[name=password]').value
 
-  var email = this.querySelector('[name=email]').value
-  var password = this.querySelector('[name=password]').value
-
-  hoodie.account.signUp({
-    username: email,
-    password: password
-  })
-
-  .then(function () {
-    return hoodie.account.signIn({
+    hoodie.account.signUp({
       username: email,
       password: password
     })
-  })
 
-  .then(function () {
-    location.href = 'index.html'
-  })
-
-  .catch(handleError)
-})
-
-/**
- * Handle signin form submit
- */
-document.querySelector('form.signin').addEventListener('submit', function (event) {
-  event.preventDefault()
-
-  var email = this.querySelector('[name=email]').value
-  var password = this.querySelector('[name=password]').value
-
-  hoodie.account.signIn({
-    username: email,
-    password: password
-  })
-
-  .then(function () {
-    location.href = 'index.html'
-  })
-
-  .catch(handleError)
-})
-
-/**
- * Handle change email form submit
- */
-document.querySelector('form.change-email').addEventListener('submit', function (event) {
-  event.preventDefault()
-
-  var email = this.querySelector('[name=email]').value
-
-  hoodie.account.update({
-    username: email
-  })
-
-  .then(function () {
-    alert('email updated')
-    showOptions()
-  })
-
-  .catch(handleError)
-})
-
-/**
- * Handle password reset form submit
- */
-document.querySelector('form.password-reset').addEventListener('submit', function (event) {
-  event.preventDefault()
-
-  var email = this.querySelector('[name=email]').value
-
-  hoodie.account.request({
-    type: 'passwordreset',
-    username: email
-  })
-
-  .then(function () {
-    alert('Email sent to ' + email)
-    document.querySelector('[data-show="password-reset"]').setAttribute('data-show', 'signin')
-    setHashState('signin')
-  })
-
-  .catch(handleError)
-})
-
-/**
- * Handle change password form submit
- */
-document.querySelector('form.change-password').addEventListener('submit', function (event) {
-  event.preventDefault()
-
-  var $passwordInput = this.querySelector('[name=password]')
-  var password = $passwordInput.value
-
-  hoodie.account.update({
-    password: password
-  })
-
-  .then(function () {
-    $passwordInput.value = ''
-    alert('password updated')
-    showOptions()
-  })
-
-  .catch(handleError)
-})
-
-/**
- * When clicking on "change username" button, set the input to current username
- */
-document.querySelector('[data-action="show-change-email"]').addEventListener('click', function (event) {
-  event.preventDefault()
-  document.querySelector('#input-change-email').value = hoodie.account.username
-})
-
-/**
- * When clicking on "forgot password?" link, preset the email input with what is
- * entered in the sign in username input.
- */
-document.querySelector('[data-action="show-password-reset"]').addEventListener('click', function (event) {
-  event.preventDefault()
-  document.querySelector('#input-email-reset').value = document.querySelector('#input-signin-email').value
-})
-
-/**
- * handle signout click. This will trigger a "signout" event which is handled
- * in common.js
- */
-document.querySelector('[data-action=signout]').addEventListener('click', function (event) {
-  event.preventDefault()
-  hoodie.account.signOut()
-
-  .then(setHashState)
-})
-
-/**
- * handle account destroy click. This will also trigger a "signout" event which
- * is handled in common.js
- */
-document.querySelector('[data-action="delete-account"]').addEventListener('click', function (event) {
-  event.preventDefault()
-  hoodie.account.destroy()
-})
-
-function showOptions () {
-  document.querySelector('.options').closest('[data-show]').setAttribute('data-show', 'options')
-}
-
-function handleError (error) {
-  alert(error)
-}
-
-function setHashState (hash) {
-  var url = window.location.origin + window.location.pathname
-  var title = originalDocumentTitle
-  if (typeof hash === 'string') {
-    url += '#' + hash
-    var formattedHash = hash
-      .split('-')
-      .map(function (string) {
-        return string.charAt(0).toUpperCase() + string.slice(1)
+    .then(function () {
+      return hoodie.account.signIn({
+        username: email,
+        password: password
       })
-      .join(' ')
-    title += ' | ' + formattedHash
-  }
-  document.title = title
-  window.history.replaceState(null, null, url)
-  window.announce(document.title + ': Page loaded', 'assertive')
-}
+    })
 
+    .then(function () {
+      window.location.href = 'index.html'
+    })
+
+    .catch(handleError)
+  })
+
+  /**
+   * Handle signin form submit
+   */
+  document.querySelector('form.signin').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var email = this.querySelector('[name=email]').value
+    var password = this.querySelector('[name=password]').value
+
+    hoodie.account.signIn({
+      username: email,
+      password: password
+    })
+
+    .then(function () {
+      window.location.href = 'index.html'
+    })
+
+    .catch(handleError)
+  })
+
+  /**
+   * Handle change email form submit
+   */
+  document.querySelector('form.change-email').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var email = this.querySelector('[name=email]').value
+
+    hoodie.account.update({
+      username: email
+    })
+
+    .then(function () {
+      alert('email updated')
+      showOptions()
+    })
+
+    .catch(handleError)
+  })
+
+  /**
+   * Handle password reset form submit
+   */
+  document.querySelector('form.password-reset').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var email = this.querySelector('[name=email]').value
+
+    hoodie.account.request({
+      type: 'passwordreset',
+      username: email
+    })
+
+    .then(function () {
+      alert('Email sent to ' + email)
+      document.querySelector('[data-show="password-reset"]').setAttribute('data-show', 'signin')
+      setHashState('signin')
+    })
+
+    .catch(handleError)
+  })
+
+  /**
+   * Handle change password form submit
+   */
+  document.querySelector('form.change-password').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var $passwordInput = this.querySelector('[name=password]')
+    var password = $passwordInput.value
+
+    hoodie.account.update({
+      password: password
+    })
+
+    .then(function () {
+      $passwordInput.value = ''
+      alert('password updated')
+      showOptions()
+    })
+
+    .catch(handleError)
+  })
+
+  /**
+   * When clicking on "change username" button, set the input to current username
+   */
+  document.querySelector('[data-action="show-change-email"]').addEventListener('click', function (event) {
+    event.preventDefault()
+    document.querySelector('#input-change-email').value = hoodie.account.username
+  })
+
+  /**
+   * When clicking on "forgot password?" link, preset the email input with what is
+   * entered in the sign in username input.
+   */
+  document.querySelector('[data-action="show-password-reset"]').addEventListener('click', function (event) {
+    event.preventDefault()
+    document.querySelector('#input-email-reset').value = document.querySelector('#input-signin-email').value
+  })
+
+  /**
+   * handle signout click. This will trigger a "signout" event which is handled
+   * in common.js
+   */
+  document.querySelector('[data-action=signout]').addEventListener('click', function (event) {
+    event.preventDefault()
+    hoodie.account.signOut()
+
+    .then(setHashState)
+  })
+
+  /**
+   * handle account destroy click. This will also trigger a "signout" event which
+   * is handled in common.js
+   */
+  document.querySelector('[data-action="delete-account"]').addEventListener('click', function (event) {
+    event.preventDefault()
+    hoodie.account.destroy()
+  })
+
+  function showOptions () {
+    document.querySelector('.options').closest('[data-show]').setAttribute('data-show', 'options')
+  }
+
+  function handleError (error) {
+    alert(error)
+  }
+
+  function setHashState (hash) {
+    var url = window.location.origin + window.location.pathname
+    var title = originalDocumentTitle
+    if (typeof hash === 'string') {
+      url += '#' + hash
+      var formattedHash = hash
+        .split('-')
+        .map(function (string) {
+          return string.charAt(0).toUpperCase() + string.slice(1)
+        })
+        .join(' ')
+      title += ' | ' + formattedHash
+    }
+    document.title = title
+    window.history.replaceState(null, null, url)
+    window.announce(document.title + ': Page loaded', 'assertive')
+  }
+})
 /* global hoodie, alert */
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,128 +1,129 @@
-var $addItemForm = document.querySelector('form.add-item')
-var $itemsList = document.querySelector('.items .list')
-var $clearButton = document.querySelector('[data-action="clear"]')
+hoodie.ready.then(function () {
+  var $addItemForm = document.querySelector('form.add-item')
+  var $itemsList = document.querySelector('.items .list')
+  var $clearButton = document.querySelector('[data-action="clear"]')
 
-/**
- * With hoodie we're storing our data locally and it will stick around next time you reload.
- * This means each time the page loads we need to find any previous notes that we have stored.
- */
-function loadAndRenderItems () {
-  hoodie.store.findAll().then(render)
-}
-
-/* render items initially on page load */
-loadAndRenderItems()
-
-/**
- * Anytime there is a data change we reload and render the list of items
- */
-hoodie.store.on('change', loadAndRenderItems)
-hoodie.store.on('clear', function () {
-  render([])
-})
-
-$clearButton.addEventListener('click', function () {
-  hoodie.store.removeAll()
-})
-
-/**
- * If you submit a form it will emit a submit event.
- * This is better than listening for a click on the submit button for example.
- * It will catch you submitting via pressing 'enter' on a keyboard or something like 'Go' on a mobile keyboard.
- * More info on form accessibility: http://webaim.org/techniques/forms/
- **/
-$addItemForm.addEventListener('submit', function (event) {
   /**
-   * By default a form will submit your form data to the page itself,
-   * this is useful if you're doing a traditional web app but we want to handle this in JavaScript instead.
-   * if we're overriding this behaviour in JavaScript we need to grab the event
-   * and prevent it from doing it's default behaviour.
-   **/
-  event.preventDefault()
+   * With hoodie we're storing our data locally and it will stick around next time you reload.
+   * This means each time the page loads we need to find any previous notes that we have stored.
+   */
+  function loadAndRenderItems () {
+    hoodie.store.findAll().then(render)
+  }
 
-  // Get values from inputs, then clear the form
-  var amount = $addItemForm.querySelector('[name=amount]').value
-  var note = $addItemForm.querySelector('[name=note]').value
-  $addItemForm.reset()
+  /* render items initially on page load */
+  loadAndRenderItems()
 
-  hoodie.store.add({
-    amount: amount,
-    note: note
+  /**
+   * Anytime there is a data change we reload and render the list of items
+   */
+  hoodie.store.on('change', loadAndRenderItems)
+  hoodie.store.on('clear', function () {
+    render([])
   })
+
+  $clearButton.addEventListener('click', function () {
+    hoodie.store.removeAll()
+  })
+
+  /**
+   * If you submit a form it will emit a submit event.
+   * This is better than listening for a click on the submit button for example.
+   * It will catch you submitting via pressing 'enter' on a keyboard or something like 'Go' on a mobile keyboard.
+   * More info on form accessibility: http://webaim.org/techniques/forms/
+   **/
+  $addItemForm.addEventListener('submit', function (event) {
+    /**
+     * By default a form will submit your form data to the page itself,
+     * this is useful if you're doing a traditional web app but we want to handle this in JavaScript instead.
+     * if we're overriding this behaviour in JavaScript we need to grab the event
+     * and prevent it from doing it's default behaviour.
+     **/
+    event.preventDefault()
+
+    // Get values from inputs, then clear the form
+    var amount = $addItemForm.querySelector('[name=amount]').value
+    var note = $addItemForm.querySelector('[name=note]').value
+    $addItemForm.reset()
+
+    hoodie.store.add({
+      amount: amount,
+      note: note
+    })
+  })
+
+  /**
+   * As items are dynamically added an removed, we cannot add event listeners
+   * to the buttons. Instead, we register a click event on the items table and
+   * then check if one of the buttons was clicked.
+   * See: https://davidwalsh.name/event-delegate
+   */
+  $itemsList.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    var action = event.target.dataset.action
+    if (!action) {
+      return
+    }
+
+    var row = event.target.parentNode.parentNode
+    var id = row.dataset.id
+    var amount = row.firstChild.textContent
+    var note = row.firstChild.nextSibling.textContent
+
+    switch (action) {
+      case 'edit':
+        row.innerHTML = '<td><input type="number" name="amount" value="' + escapeHtml(amount) + '" data-reset-value="' + escapeHtml(amount) + '"></td>' +
+                        '<td><input type="text" name="note" value="' + escapeHtml(note) + '" data-reset-value="' + escapeHtml(note) + '"></td>' +
+                        '<td><a href="#" data-action="update">Save</a></td><td><a href="#" data-action="cancel">Cancel</a></td>'
+        break
+      case 'cancel':
+        loadAndRenderItems()
+        break
+
+      case 'remove':
+        hoodie.store.remove({
+          id: id
+        })
+        break
+      case 'update':
+        amount = row.querySelector('input[name=amount]').value
+        note = row.querySelector('input[name=note]').value
+        hoodie.store.update(id, {
+          amount: amount,
+          note: note
+        })
+    }
+  })
+
+  function render (items) {
+    if (items.length === 0) {
+      document.body.setAttribute('data-store-state', 'empty')
+      return
+    }
+
+    document.body.setAttribute('data-store-state', 'not-empty')
+    $itemsList.innerHTML = items
+      .sort(orderByCreatedAt)
+      .map(function (item) {
+        return '<tr data-id="' + item.id + '">' +
+               '<td>' + escapeHtml(item.amount) + '</td>' +
+               '<td>' + escapeHtml(item.note) + '</td>' +
+               '<td><a href="#" data-action="edit">Edit</a></td>' +
+               '<td><a href="#" data-action="remove">Delete</a></td>' +
+               '</tr>'
+      }).join('')
+  }
+
+  function orderByCreatedAt (item1, item2) {
+    return item1.createdAt < item2.createdAt ? 1 : -1
+  }
+
+  function escapeHtml (unsafeHtml) {
+    var text = document.createTextNode(unsafeHtml)
+    var div = document.createElement('div')
+    div.appendChild(text)
+    return div.innerHTML
+  }
 })
-
-/**
- * As items are dynamically added an removed, we cannot add event listeners
- * to the buttons. Instead, we register a click event on the items table and
- * then check if one of the buttons was clicked.
- * See: https://davidwalsh.name/event-delegate
- */
-$itemsList.addEventListener('click', function (event) {
-  event.preventDefault()
-
-  var action = event.target.dataset.action
-  if (!action) {
-    return
-  }
-
-  var row = event.target.parentNode.parentNode
-  var id = row.dataset.id
-  var amount = row.firstChild.textContent
-  var note = row.firstChild.nextSibling.textContent
-
-  switch (action) {
-    case 'edit':
-      row.innerHTML = '<td><input type="number" name="amount" value="' + escapeHtml(amount) + '" data-reset-value="' + escapeHtml(amount) + '"></td>' +
-                      '<td><input type="text" name="note" value="' + escapeHtml(note) + '" data-reset-value="' + escapeHtml(note) + '"></td>' +
-                      '<td><a href="#" data-action="update">Save</a></td><td><a href="#" data-action="cancel">Cancel</a></td>'
-      break
-    case 'cancel':
-      loadAndRenderItems()
-      break
-
-    case 'remove':
-      hoodie.store.remove({
-        id: id
-      })
-      break
-    case 'update':
-      amount = row.querySelector('input[name=amount]').value
-      note = row.querySelector('input[name=note]').value
-      hoodie.store.update(id, {
-        amount: amount,
-        note: note
-      })
-  }
-})
-
-function render (items) {
-  if (items.length === 0) {
-    document.body.setAttribute('data-store-state', 'empty')
-    return
-  }
-
-  document.body.setAttribute('data-store-state', 'not-empty')
-  $itemsList.innerHTML = items
-    .sort(orderByCreatedAt)
-    .map(function (item) {
-      return '<tr data-id="' + item.id + '">' +
-             '<td>' + escapeHtml(item.amount) + '</td>' +
-             '<td>' + escapeHtml(item.note) + '</td>' +
-             '<td><a href="#" data-action="edit">Edit</a></td>' +
-             '<td><a href="#" data-action="remove">Delete</a></td>' +
-             '</tr>'
-    }).join('')
-}
-
-function orderByCreatedAt (item1, item2) {
-  return item1.createdAt < item2.createdAt ? 1 : -1
-}
-
-function escapeHtml (unsafeHtml) {
-  var text = document.createTextNode(unsafeHtml)
-  var div = document.createElement('div')
-  div.appendChild(text)
-  return div.innerHTML
-}
-
 /* global hoodie */
