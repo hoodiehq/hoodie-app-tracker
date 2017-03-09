378be182839cac11acdd6465169e854af9a358ae
Gregor <gregor@martynus.net>
2017-03-09 01:34:49 -0800

    fix: `hoodie@28`

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,22 +1,23 @@
 /* global hoodie */
-hoodie.ready.then(function () {
-  function showSignedIn (username) {
-    document.querySelector('[data-value=username]').textContent = username
-    document.body.setAttribute('data-account-state', 'signed-in')
-  }
+function showSignedIn (username) {
+  document.querySelector('[data-value=username]').textContent = username
+  document.body.setAttribute('data-account-state', 'signed-in')
+}
 
-  function hideSignedIn () {
-    document.body.setAttribute('data-account-state', 'signed-out')
-  }
+function hideSignedIn () {
+  document.body.setAttribute('data-account-state', 'signed-out')
+}
 
-  hoodie.account.on('signin', function (account) {
-    showSignedIn(account.username)
-  })
-
-  hoodie.account.on('signout', hideSignedIn)
-  if (hoodie.account.isSignedIn()) {
-    showSignedIn(hoodie.account.username)
-  } else {
-    hideSignedIn()
-  }
+hoodie.account.on('signin', function (account) {
+  showSignedIn(account.username)
 })
+
+hoodie.account.on('signout', hideSignedIn)
+hoodie.account.get(['session', 'username'], {local: true})
+  .then(function (properties) {
+    if (properties.session) {
+      showSignedIn(properties.username)
+    } else {
+      hideSignedIn()
+    }
+  })
