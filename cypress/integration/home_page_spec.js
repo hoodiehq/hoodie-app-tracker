d27a300727a1dff78e63ff7900c287713bbcb21e
fozy81 <fozy81@github.com>
2017-10-24 00:20:33 +0100

    cypress testing - start server

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,14 @@
-describe('The Home Page', function() {
-  it('successfully loads', function() {
-     cy.visit('/')
+module.exports = startHoodie
+
+var hoodieCli = require('hoodie/cli')
+
+function startHoodie (callback) {
+  process.env.hoodie_inMemory = true
+  hoodieCli(callback)
+}
+
+describe('The Home Page', function () {
+  it('successfully loads', function () {
+    cy.visit('/')
   })
 })
